ca3483e3d099fdab0a08a2890cfc735041f5fa07
Joel Devey <deveyjoel@gmail.com>
2021-10-30 22:16:46 -0600

    Problem 86

--- Problem_086/main.cpp
+++ Problem_086/main.cpp
@@ -1,10 +1,4 @@
-5x6, 6 x 3
-l1 = sqrt(5^2 + n^2)
-l2 = sqrt((6 - n)^2 + 3^2)
-d = sqrt(5^2 + n^2) + sqrt((6 - n)^2 + 3^2)
-d' = 1/2 * (25 + n^2) ^ (-1/2) * (2n) + 1/2 * (n^2 - 12n + 36 + 9) ^ (-1/2) * (-12 + 2n)
-= 2n/(2 * sqrt(25 + n^2)) + (-12 + 2n) / (2 * sqrt(n^2 - 12n + 36 + 9))
-
+/*
 l1 = sqrt(a^2 + n^2)
 l2 = sqrt((b - n)^2 + c^2)
 d = sqrt(a^2 + n^2) + sqrt((b - n)^2 + c^2)
@@ -37,10 +31,50 @@ for +:
 for -:
 = (2a^2b - 2abc) / 2(a^2-c^2)
 = 2ab(a - c) / 2(a^2-c^2)
-= 2ab(a + c) / 2(a - c)(a + c)
+= 2ab(a - c) / 2(a - c)(a + c)
 = 2ab / 2(a + c)
-= ab / (a + c)
+= ab / (a + c) (local minimum)
 
 def cuboid_route_length(a, b, c):
-	p = ab / (a - c)
-	return sqrt(a^2 + p^2) + sqrt(c^2 + (b - p)^2)
+	p = a * b / (a + c)
+	return math.sqrt(a * a + p * p) + math.sqrt(c * c + (b - p) * (b - p))
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double cuboid_route_length(double a, double b, double c) {
+	double p = a * b / (a + c);
+	return sqrt(a * a + p * p) + sqrt(c * c + (b - p) * (b - p));
+}
+
+pair <double, bool> compute_route(double a, double b, double c) {
+	double l = cuboid_route_length(a, b, c);
+	return {l, abs(l - round(l)) < 1e-6};
+}
+
+bool is_integral_combination(double a, double b, double c) {
+	return min({compute_route(a, b, c), compute_route(b, a, c), compute_route(a, c, b)}).second;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int sm = 0;
+	for (int i = 1;; ++i) {
+		for (int j = 1; j <= i; ++j) {
+			for (int k = 1; k <= j; ++k) {
+				bool is_integral_min = is_integral_combination(i, j, k);
+				if (is_integral_min) {
+					++sm;
+					if (sm == 1000000) {
+						cout << i << endl;
+						return 0;
+					}
+				}
+			}
+		}
+		cout << i << ' ' << sm << endl;
+	}
+	return 0;
+}
